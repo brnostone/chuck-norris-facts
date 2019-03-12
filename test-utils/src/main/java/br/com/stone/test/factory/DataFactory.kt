c55a29fe26aae21cf18d088ddb3218fec4971c4b
Bruno Stone <brnostone@gmail.com>
2019-03-12 17:17:49 -0300

    proguard & remove unused data factory

--- test-utils/src/main/java/br/com/stone/test/factory/DataFactory.kt
+++ test-utils/src/main/java/br/com/stone/test/factory/DataFactory.kt
@@ -5,22 +5,8 @@ import java.util.concurrent.ThreadLocalRandom
 
 object DataFactory {
 
-    const val EMAIL = "user@email.com"
-
     fun randomString(): String {
         return UUID.randomUUID().toString()
-    }
-
-    fun randomInt(): Int {
-        return ThreadLocalRandom.current().nextInt(0, 1000 + 1)
-    }
-
-    fun randomLong(): Long {
-        return randomInt().toLong()
-    }
-
-    fun randomBoolean(): Boolean {
-        return Math.random() < 0.5
     }
 
 }
